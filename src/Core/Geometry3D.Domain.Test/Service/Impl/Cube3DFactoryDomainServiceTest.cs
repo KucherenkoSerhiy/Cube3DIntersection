8f243bcc1129e85144f7f0b96e283bdd59e95eae
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2020-11-12 11:36:35 +0100

    Added ICube3DFactoryDomainService interface, and its auto generated boilerplate implementation and test

---
+++ src/Core/Geometry3D.Domain.Test/Service/Impl/Cube3DFactoryDomainServiceTest.cs
@@ -0,0 +1,26 @@
+﻿namespace Geometry3D.Domain.Test.Service.Impl
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Cube3DFactoryDomainServiceTest
+    {
+        [Test]
+        public void InstantiateFromOppositeVerticesTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void InstantiateFromOppositeEdgesTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void InstantiateFromOppositePlanesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ src/Core/Geometry3D.Domain/Service/Impl/Cube3DFactoryDomainService.cs
@@ -0,0 +1,23 @@
+﻿namespace Geometry3D.Domain.Service.Impl
+{
+    using Model;
+    using Model.Shape;
+
+    public class Cube3DFactoryDomainService: ICube3DFactoryDomainService
+    {
+        public Cube3D InstantiateFromOppositeVertices(Dot3D a, Dot3D b)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Cube3D InstantiateFromOppositeEdges(Vector3D a, Vector3D b)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Cube3D InstantiateFromOppositePlanes(Plane3D a, Plane3D b)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
