85d336105bb587d4c9923f37a538a791f19ca7fb
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2020-11-12 11:27:32 +0100

    added a test for Cube3D model and a default constructor which initializes its list objects

---
+++ src/Core/Geometry3D.Domain.Test/Model/Shape/Cube3DTest.cs
@@ -0,0 +1,21 @@
+﻿namespace Geometry3D.Domain.Test.Model.Shape
+{
+    using Domain.Model.Shape;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Cube3DTest
+    {
+        [Test]
+        public void Id_Ok()
+        {
+            string referenceId = "a cube";
+            int vertices = 0;
+            string expected = $"{referenceId}{vertices}";
+            Cube3D cube = new Cube3D {ReferenceId = referenceId};
+
+            Assert.IsNotNull(cube);
+            Assert.AreEqual(expected, cube.Id);
+        }
+    }
+}
--- src/Core/Geometry3D.Domain/Model/Shape/Cube3D.cs
+++ src/Core/Geometry3D.Domain/Model/Shape/Cube3D.cs
@@ -1,8 +1,17 @@
 ﻿namespace Geometry3D.Domain.Model.Shape
 {
+    using System.Collections.Generic;
+
     public class Cube3D: Shape3D
     {
         public string ReferenceId { get; set; }
         public override string Id => $"{this.ReferenceId}{this.Vertices.Count}";
+
+        public Cube3D()
+        {
+            this.Vertices = new List<Dot3D>();
+            this.Edges = new List<Vector3D>();
+            this.Planes = new List<Plane3D>();
+        }
     }
 }
