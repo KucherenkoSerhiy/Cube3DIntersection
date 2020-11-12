2097b6f4783a2523d63ce1ef68fbce94b62e1914
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2020-11-12 11:32:29 +0100

    added new factory interface ICube3DFactoryDomainService

---
+++ src/Core/Geometry3D.Domain/Service/ICube3DFactoryDomainService.cs
@@ -0,0 +1,12 @@
+﻿namespace Geometry3D.Domain.Service
+{
+    using Model;
+    using Model.Shape;
+
+    public interface ICube3DFactoryDomainService
+    {
+        Cube3D InstantiateFromOppositeVertices(Dot3D a, Dot3D b);
+        Cube3D InstantiateFromOppositeEdges(Vector3D a, Vector3D b);
+        Cube3D InstantiateFromOppositePlanes(Plane3D a, Plane3D b);
+    }
+}
