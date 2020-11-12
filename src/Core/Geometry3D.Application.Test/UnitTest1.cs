d0f4491493ac869f8ae1ed40cc94d6d03de0c14f
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2020-11-12 11:19:32 +0100

    added test projects

---
+++ src/Core/Geometry3D.Application.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Geometry3D.Application.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
